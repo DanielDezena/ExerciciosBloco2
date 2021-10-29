f7e4ede112446a2fa5157785470552cc3fd8e55c
jo se <danieldezena098@gmail.com>
2021-10-29 14:23:36 -0300

    Atualização com os métodos adicionados post, put e delete

--- AtividadeCRUD/postgen/postgen/src/main/java/org/generation/blogpessoal/postgen/controller/PostagemController.java
+++ AtividadeCRUD/postgen/postgen/src/main/java/org/generation/blogpessoal/postgen/controller/PostagemController.java
@@ -2,19 +2,24 @@ package org.generation.blogpessoal.postgen.controller;
 
 import java.util.List;
 
+import org.generation.blogpessoal.postgen.model.Postagem;
+import org.generation.blogpessoal.postgen.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.generation.blogpessoal.postgen.model.Postagem;
-import org.generation.blogpessoal.postgen.repository.PostagemRepository;
-
 @RestController
 @RequestMapping("/postagens")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "*")
 public class PostagemController {
 	
 	@Autowired
@@ -22,6 +27,33 @@ public class PostagemController {
 	@GetMapping
 	public ResponseEntity<List<Postagem>> getAll(){
 		return ResponseEntity.ok(postagemRepository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable long id){
+		return postagemRepository.findById(id)
+		.map(resp -> ResponseEntity.ok(resp))
+		.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitle(@PathVariable String titulo){
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Postagem> post (@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Postagem> put (@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		postagemRepository.deleteById(id);
 	}
 
 
--- AtividadeCRUD/postgen/postgen/src/main/java/org/generation/blogpessoal/postgen/repository/PostagemRepository.java
+++ AtividadeCRUD/postgen/postgen/src/main/java/org/generation/blogpessoal/postgen/repository/PostagemRepository.java
@@ -1,12 +1,16 @@
 package org.generation.blogpessoal.postgen.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
 import org.generation.blogpessoal.postgen.model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 	
 	@Repository
 	public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+		
+		public List <Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+			
 		
 	}
 
