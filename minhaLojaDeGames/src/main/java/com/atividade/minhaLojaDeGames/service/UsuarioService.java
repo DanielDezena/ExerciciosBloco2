80e2fac1916885206363b0cfd0b6d5ba109aafa4
jo se <danieldezena098@gmail.com>
2021-11-17 23:17:51 -0300

    atualização com o security

--- minhaLojaDeGames/src/main/java/com/atividade/minhaLojaDeGames/service/UsuarioService.java
+++ minhaLojaDeGames/src/main/java/com/atividade/minhaLojaDeGames/service/UsuarioService.java
@@ -17,6 +17,11 @@ public class UsuarioService {
 
 	@Autowired
 	private UsuarioRepository usuarioRepository;
+	
+	/*
+	 * checa se possui um usuário com o mesmo email digitado
+	 * caso não tiver criará uma nova conta
+	 */
 
 	public Optional<Usuario> cadastrarUsuario(Usuario usuario) {
 		if (usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent())
@@ -24,6 +29,11 @@ public class UsuarioService {
 		usuario.setSenha(criptografarSenha(usuario.getSenha()));
 		return Optional.of(usuarioRepository.save(usuario));
 	}
+	
+	/*
+	 * checa se existe uma conta com o id e o email digitado pelo usuario
+	 * se tiver a conta poderá atualizar o nome e/ou a senha do login
+	 */
 
 	public Optional<Usuario> atualizarUsuario(Usuario usuario) {
 		if (usuarioRepository.findById(usuario.getId()).isPresent()) {
@@ -36,6 +46,11 @@ public class UsuarioService {
 		}
 		return Optional.empty();
 	}
+	
+	/*
+	 * checa se a senha do login é a mesma da senha do cadastro.
+	 * se for verdadeiro retornará as informações do usuário e o token criado
+	 */
 
 	public Optional<UsuarioLogin> autenticarUsuario(Optional<UsuarioLogin> usuarioLogin) {
 		Optional<Usuario> usuario = usuarioRepository.findByUsuario(usuarioLogin.get().getUsuario());
@@ -53,20 +68,32 @@ public class UsuarioService {
 
 		return Optional.empty();
 	}
+	/*
+	 * retorna a senha digitada pelo usuário encriptografada
+	 */
 
 	private String criptografarSenha(String senha) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		return encoder.encode(senha);
 	}
 
+	/*
+	 * checa se a senha digitada é igual a senha registrada
+	 */
+	
 	private boolean compararSenhas(String senhaDigitada, String senhaBanco) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		return encoder.matches(senhaDigitada, senhaBanco);
 	}
+	
+	/*
+	 * cria e retorna o token para o usuário criptografado em base64
+	 * exemplo: "basic ********************"
+	 */
 
 	private String gerarBasicToken(String email, String password) {
 		String tokenBase = email + ":" + password;
-		byte[] tokenBase64 = Base64.encodeBase64(tokenBase.getBytes(Charset.forName("US=ASCII")));
+		byte[] tokenBase64 = Base64.encodeBase64(tokenBase.getBytes(Charset.forName("US-ASCII")));
 		return "Basic " + new String(tokenBase64);
 	}
 
